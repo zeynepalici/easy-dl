a3eef26354c874a614956f432032108674c25162
Zeynep Alici <aliciizeynep@gmail.com>
2020-07-24 22:59:32 +0300

    initialize parameters

--- easy_dl.py
+++ easy_dl.py
@@ -3,31 +3,36 @@ import numpy as np
 
 
 class EasyDL:
-    def __init__(self, filename, layers=0, units=None, activations=None):
+    def __init__(self, filename, layers=0, neurons=None, activations=None):
         data = pd.read_csv(filename).to_numpy()  # pandas data frame döndürüyor. biz numpy kullanmak istiyoruz.
 
         self.X = data[:, :len(data[0]) - 1]
         self.Y = data[:, len(data[0]) - 1]
-        self.features = len(self.X[0])
+        self.number_of_features = len(self.X[0])
 
         self.layers = layers
-        self.units = units
+        self.neurons = neurons
+        self.neurons.insert(0, self.number_of_features)
         self.activations = activations
 
     def learn(self):
-        pass
+        weight, b_values = self._initialize_parameters()
+        print(weight)
+        print("------------------------------")
+        print(b_values)
 
-    def _initialize_weights(self):
+    def _initialize_parameters(self):
         weights = []
-        for i in range(self.layers):
-            if i == 0:
-                current_weight = np.random.rand(self.units[i], self.features)
-            else:
-                current_weight = np.random.rand(self.units[i], self.units[i - 1])
+        b_values = []
+
+        for i in range(1, self.layers+1):
+            current_weight = np.random.rand(self.neurons[i], self.neurons[i - 1])
+            current_b = np.random.rand(self.neurons[i], 1)
 
             weights.append(current_weight)
+            b_values.append(current_b)
 
-        return weights
+        return weights, b_values
 
     def _forward_propagation(self):
         w = []
@@ -36,6 +41,7 @@ class EasyDL:
         for i in range(self.layers):
             if i == 0:
                 Z = np.dot(w[i], self.X) + b[i]
-                a.append() #function yaz, z yi ve suanki activation ı gönder
+                a.append()  # function yaz, z yi ve suanki activation ı gönder
+                # w[0] :
             else:
-                Z = np.dot(w[i], a[i-1]) + b[i]
+                Z = np.dot(w[i], a[i - 1]) + b[i]
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import easy_dl
 
 if __name__ == "__main__":
-    model = easy_dl.EasyDL("temp.csv", layers=2, units=[3, 3], activations=["relu", "sigmoid"])
-    #model.learn()
+    model = easy_dl.EasyDL("temp.csv", layers=2, neurons=[3, 1], activations=["relu", "sigmoid"])
+    model.learn()
 
+    # 2 tane w: w1: 3,4  w2: 1,3 b1: 3,1 b2: 1,1
     #test_results = model.test("test.csv")
     #print(test_results)
 
